ed4a123f5562aa75741b6c9b7fb0ee79b84f4a49
RyanPintoPassos <119443198+RyanPintoPassos@users.noreply.github.com>
2023-06-07 14:56:39 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,42 +1,48 @@
-//https://teachablemachine.withgoogle.com/models/JGwAuBrVI/
+Webcam.attach( '#camera' );
 
-    Webcam.set({
+camera = document.getElementById("camera");
+      
+  Webcam.set({
     width:350,
     height:300,
     image_format : 'png',
     png_quality:90
+  });
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
     });
+}
 
-    camera = document.getElementById("camera");
+  console.log('ml5 version:', ml5.version);
+  
+  // Initialize the Image Classifier method with MobileNet
+//classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JACkTMciY/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JGwAuBrVI/model.json',modelLoaded);
 
-Webcam.attach( '#camera' );
-
-    function take_snapshot()
-    {
-        Webcam.snap(function(data_uri){
-            document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'">';
-        });
-    }
-    console.log('versão ml5:' , ml5.version);
-    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JGwAuBrVI/model.json',modelLoaded);
-
-    function modelLoaded() {
-        console.log('Model Loaded!');
-    } 
-
-    function check()
-    {
-        img = document.getElementById('selfie_image');
-        classifier.classify(img, gotResult);
-    }
+  // When the model is loaded
+  function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+      
+  function check()
+  {
+    img = document.getElementById('selfie_image');
+    classifier.classify(img, gotResult);
+  }
 
 
-    function gotResult(error, results) {
-        if (error) {
-            console.error(error);
-        } else {
-            console.log(results);
-            document.getElementById("result_object_name").innerHTML = results[0].label;
-            document.getElementById("reuslt_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
-        }
-    }
+// A function to run when we get any errors and the results
+function gotResult(error, results) {
+  // Display error in the console
+  if (error) {
+    console.error(error);
+  } else {
+    // The results are in an array ordered by confidence.
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+  }
+}
